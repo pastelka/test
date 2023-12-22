32a1feabb0e920884763b662658920160e1a8dd5
delpsys <pavel.zakavec@delpsys.cz>
2023-12-22 20:36:29 +0100

    renamed:    goodwe.py -> read_goodwe.py 	modified:   readxml.py

--- readxml.py
+++ readxml.py
@@ -1,6 +1,6 @@
 """Test cteni xml dat"""
-import xml.etree.ElementTree as ET
 import requests
+import xml.etree.ElementTree as ET
 
 response = requests.get("http://10.1.2.11/XMLstatus", timeout=30)
 root = ET.fromstring(response.text)
