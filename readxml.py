4464298c2fb22bb4ec4c576c5e4cec22fa25d69c
delpsys <pavel.zakavec@delpsys.cz>
2023-12-22 18:03:17 +0100

    modified:   readxml.py

--- readxml.py
+++ readxml.py
@@ -5,4 +5,4 @@ import requests
 response = requests.get("http://10.1.2.11/XMLstatus", timeout=30)
 root = ET.fromstring(response.text)
 print(root.find('.//WaterTankVolume').text)
-print(root.find('.//TeperatureOut').text)
+print(root.find('.//TemperatureOut').text)
